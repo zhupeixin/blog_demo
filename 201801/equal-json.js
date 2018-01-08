ebffb1e36014a722bb7336eb3b47f011ced19834
zhupeixin_sx <zhupeixin_sx@qiyi.com>
2018-01-08 12:52:52 +0800

    修复传入值为非json时产生的bug

--- 201801/equal-json.js
+++ 201801/equal-json.js
@@ -1,13 +1,18 @@
 //本文地址：http://zhupeixin.top/article/2018/01/equalobj
+var objC = {}.constructor;
+function isObj(obj) {
+    return obj && obj.constructor === objC;
+}
+
 var arrC = [].constructor;
 var isArr = function (arr) {
-    return arr.constructor === arrC;
+    return arr && arr.constructor === arrC;
 };
 
 function compare(obj1, obj2) {
     var flag = true;
-    if (typeof obj1 === 'object' && typeof obj2 === 'object') {
-        if (isArr(obj1) && isArr(obj2) && obj1.length !== obj2.length) return false;
+    if ((isObj(obj1) && isObj(obj2)) || (isArr(obj1) && isArr(obj2))) {
+        if (isArr(obj1) && obj1.length !== obj2.length) return false;
         for (var key in obj1) {
             if (typeof obj1[key] === 'object') {
                 if (!compare(obj1[key], obj2[key]))
