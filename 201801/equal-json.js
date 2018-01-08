4ae0123fbd90307944713c0b5f67d3324a4dad21
zhupeixin_sx <zhupeixin_sx@qiyi.com>
2018-01-08 15:25:04 +0800

    修复传入数据为空时的bug

--- 201801/equal-json.js
+++ 201801/equal-json.js
@@ -1,12 +1,12 @@
 //本文地址：http://zhupeixin.top/article/2018/01/equalobj
 var objC = {}.constructor;
 function isObj(obj) {
-    return obj && obj.constructor === objC;
+    return !!(obj && obj.constructor === objC);
 }
 
 var arrC = [].constructor;
 var isArr = function (arr) {
-    return arr && arr.constructor === arrC;
+    return !!(arr && arr.constructor === arrC);
 };
 
 function compare(obj1, obj2) {
